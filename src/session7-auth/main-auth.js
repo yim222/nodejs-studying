80012cc2a58b49ae5f94a19b43348955b55906d7
lingar <yimprogramming@gmail.com>
2022-03-13 14:14:10 +0200

    Validators of mongoose working

--- src/session7-auth/main-auth.js
+++ src/session7-auth/main-auth.js
@@ -18,3 +18,34 @@ console.log("Hi authentication example");
 const ws = require("./ws/web-services");
 
 ws();
+//TODO - NOW
+//now, let's say that I want it to be authenticated. This means that only sign-in user will be able to add a stock.
+
+//need to install : https://www.npmjs.com/package/jsonwebtoken -->For creating json web token object. For example:
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjJiMzdjOGEzNzM0YThhYjQ2OTE0N2EiLCJpYXQiOjE2NDcxNzEyMDV9.VrGKo00_eKngboXOn9RpesZcPolUSX4ppp8DG-40l0U
+//that too: https://www.npmjs.com/package/joi --> For validate provided data vs, what we want.
+
+//Steps:
+//validation examples
+
+
+
+
+//TODO - Later
+//Make the request with user.
+//Make sign out, make expiring to the token
+
+
+//we also need to install "cors" package, it's for later, when we'll run client side app from the localhost.
+//npm i cors
+//https://www.npmjs.com/package/cors
+
+//Also later... How (and for what) to crypt a password
+//https://www.npmjs.com/package/bcrypt
+//https://www.npmjs.com/package/joi-password-complexity
+//lodash - for what and how .
+
+//Mongoose - validate schema:
+//https://mongoosejs.com/docs/validation.html
+
+
--- src/session7-auth/ws/web-services.js
+++ src/session7-auth/ws/web-services.js
@@ -1,4 +1,3 @@
-
 let startWebServices = async () => {
 
     console.log("Starting WS");
@@ -35,9 +34,15 @@ let startWebServices = async () => {
         this.price = price;
         this.isUp = isUp;
     }
+
     // instance = שכפול
     let stockInstance1 = new Stock("TSLA", 839.29, false);
-    const stockSchema = new mongoose.Schema({ sign: String, price: Number, isUp: Boolean, creator: mongoose.Schema.ObjectId });
+    const stockSchema = new mongoose.Schema({
+        sign: String,
+        price: Number,
+        isUp: Boolean,
+        creator: mongoose.Schema.ObjectId
+    });
 
     const StockMongo = mongoose.model('Stock', stockSchema);
 
@@ -51,6 +56,14 @@ let startWebServices = async () => {
     });
 
     app.post("/stocks", (req, res, next) => {
+        let isUserSignIn = false;
+        if (!isUserSignIn) {
+            res.send("User is not sign in");
+            return;
+
+
+        }
+        console.log("after send")
         let data = req.body;
         let stockInstance2 = new Stock(data.sign, data.price, data.isUp);//
         console.log(stockInstance2);
@@ -68,6 +81,88 @@ let startWebServices = async () => {
     app.delete("/stocks/:id", (req, res, next) => {
         //todo
     });
+
+
+    //making users:
+    //User Object:
+    function User(email, password) {
+        this.email = email;
+        this.password = password;
+    }
+/*
+name: {
+    type: String,
+    require: true,
+    minlength: 4,
+    maxlength: 50,
+  },
+ */
+    //user schema:
+    const userSchema = new mongoose.Schema({
+        email: {
+            type: String,
+            require: true,
+            minLength: 4
+            // maxLength
+        },
+        password: {
+            type: String,
+            require: true,
+            minLength: 4,
+            maxLength: 8
+        },
+        creator: mongoose.Schema.ObjectId
+    });
+    const UserMongo = mongoose.model('User', userSchema);
+
+
+    app.get("/users", async (req,res, next)=>{
+        let users = await UserMongo.find();
+        res.send(users);
+    });
+
+    //user js schema
+
+    app.post("/users", (req, res, next) => {
+
+        let data = req.body;
+        let userInstance = new User(data.email, data.password);//
+
+
+        console.log(userInstance);
+
+
+        let userDbConnection = new UserMongo(userInstance);
+
+        //U here - there is a problem with the validation here .
+        userDbConnection.save().then(() => {
+            console.log("user added successfully ", userInstance);
+        });
+
+        res.send("user added successfully");
+    });
+
+
+    //running:
+
+    try {
+        let userInstance1 = new User("bbb23","33344");//
+        let userDbConnection1 = new UserMongo(userInstance1);
+        userDbConnection1.save().catch((err)=>{
+            console.error("error = \n", err);
+
+        });
+    }catch (err){
+        console.error("error2 = \n", err);
+
+    }
+
+
+
+
+
+
+
 }
 
 
